65d2cbc573ebc2060c7badb73e82519ff50c2001
Natalia Aleksandra Derwecka <derwecka@KOMP313-11.dydaktyka.wszib.edu.pl>
2022-10-16 14:19:32 +0200

    app impl & static field

--- src/main/java/pl/edu/wszib/app/Application.java
+++ src/main/java/pl/edu/wszib/app/Application.java
@@ -1,6 +1,21 @@
 package pl.edu.wszib.app;
 
+import pl.edu.wszib.app.mypackage.MyClass2;
+
 public class Application {
 
+    public static void main(String[] args) {
+        System.out.println("Hello world");
 
+        String myStaticField = MyClass2.MY_STATIC_FIELD;
+
+        MyClass2 myClass = new MyClass2();
+        myClass.field3 = "Field3";
+        myClass.field4 = "Field4";
+
+        myClass.run3();
+        myClass.run4();
+
+        myClass.run();
+    }
 }
--- src/main/java/pl/edu/wszib/app/mypackage/MyClass2.java
+++ src/main/java/pl/edu/wszib/app/mypackage/MyClass2.java
@@ -6,6 +6,7 @@ import pl.edu.wszib.app.MyInterface2;
 
 public class MyClass2 extends MyClass implements MyInterface, MyInterface2 {
 //    public MyClass myClass;
+    public static final String MY_STATIC_FIELD = "My static field";
     @Override
     public void run() {
         field3 = "Field3 from extended";
